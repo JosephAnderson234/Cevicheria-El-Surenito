a12925ca860377b4a353ba3cedd237a9c046f6a1
JosephAnderson234 <coserojasj@gmail.com>
2023-03-26 10:47:35 -0500

    Ending the main styles
    At this moment we have finished the styles and the structure of the header, footer and the flying social bar.

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,12 @@ const nav = document.querySelector("nav")
 button.addEventListener("click", ()=>{
     nav.classList.toggle("show")
 })
+
+
+const siButton = document.querySelector(".SI");
+const socialBar = document.querySelector(".social-links-ul");
+
+siButton.addEventListener("click", ()=>{
+    siButton.classList.toggle("show")
+    socialBar.classList.toggle("moved")
+})
