485bb98cbe13cae5bf77e9db62f225e807d6e33b
JosephAnderson234 <coserojasj@gmail.com>
2023-08-15 18:54:30 -0500

    Final Version Of all the minimun details

--- scripts/home.js
+++ scripts/home.js
@@ -14,18 +14,41 @@ function clearHeader() {
 }
 
 window.addEventListener("scroll", clearHeader)
-setTimeout(clearHeader, 0000)
+setTimeout(clearHeader, 0)
 
 const buttonGalleryB = document.getElementsByClassName("before");
 const buttonGalleryA = document.getElementsByClassName("after");
 
-
 buttonGalleryB[0].addEventListener("click", ()=>{
     var left = document.querySelector(".scroll-images");
-    left.scrollBy(-1111, 0);
+    left.scrollBy(-screen.width, 0);
 })
 
 buttonGalleryA[0].addEventListener("click", ()=>{
     var left = document.querySelector(".scroll-images");
-    left.scrollBy(1111, 0);
+    left.scrollBy(screen.width, 0);
 })
+
+function left (){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(screen.width, 0)
+}
+function right (){
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(-screen.width, 0)
+}
+
+/* function animationHeader(){
+    for(let j = 0; j < 100; j++) {
+        for (let i = 0; i <= 2; i++) {
+            setTimeout(right, 5000)
+        }
+        for (let i = 0; i <= 2; i++) {
+            setTimeout(right, 5000)
+        }
+    }
+    
+    
+}
+
+window.addEventListener("load", animationHeader) */
